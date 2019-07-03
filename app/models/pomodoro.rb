d378ef93501723911b112ef73a9a2df32789a8f5
Oscar Bernardini <bernardini687@gmail.com>
2019-07-03 14:56:08 +0200

    Call job after saving a Pomodoro instance

--- app/models/pomodoro.rb
+++ app/models/pomodoro.rb
@@ -4,4 +4,12 @@ class Pomodoro < ApplicationRecord
     greater_than_or_equal_to: 0,
     less_than_or_equal_to: 4
   }
+
+  after_save :start_timer
+
+  private
+
+  def start_timer
+    count.times { TestJob.perform_later }
+  end
 end
