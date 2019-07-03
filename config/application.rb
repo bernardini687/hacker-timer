2d3a9826bdd57f512259dec95a48bb500abd8a0c
Oscar Bernardini <bernardini687@gmail.com>
2019-07-03 16:35:19 +0200

    Remove automatic test generation in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module HackerTimer
     config.generators do |generate|
           generate.assets false
           generate.helper false
-          generate.test_framework  :test_unit, fixture: false
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
