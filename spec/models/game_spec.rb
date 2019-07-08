25edb0beece5e64d8f52a77a68aef77d8b096f23
Oscar Bernardini <bernardini687@gmail.com>
2019-07-08 14:22:59 +0200

    Refine models specifications

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,23 +3,25 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
   it 'is accessible' do
     game = Game.create!(number_of_players: 5)
+
     expect(game).to eq(Game.last)
   end
 
   it 'has number_of_players column' do
     columns = Game.column_names
+
     expect(columns).to include('id')
     expect(columns).to include('number_of_players')
-    expect(columns).not_to include('user_id')
   end
 
-  it 'has number_of_players column that defaults to 3' do
+  it 'has a number of players that defaults to 3' do
     expect(Game.new.number_of_players).to eq 3
   end
 
   it 'validates number_of_players' do
     expect(Game.new(number_of_players: 2)).not_to be_valid
     expect(Game.new(number_of_players: 9)).not_to be_valid
+
     expect(Game.new(number_of_players: 5)).to be_valid
     expect(Game.new).to be_valid
   end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -2,14 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Player, type: :model do
   it 'is accessible' do
-    player = Player.create!
+    game = Game.create!
+    player = Player.create!(game: game)
+
     expect(player).to eq(Player.last)
   end
 
   it 'has info column' do
     columns = Player.column_names
+
     expect(columns).to include('id')
     expect(columns).to include('info')
     expect(columns).to include('game_id')
+  end
+
+  it 'validates game presence' do
+    game = Game.create!
+
+    expect(Player.new).not_to be_valid
+    expect(Player.new(game: game))
   end
 end
