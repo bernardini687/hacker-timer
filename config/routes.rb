688035b0bd5406d622730b134963b3a9968e74f1
Oscar Bernardini <bernardini687@gmail.com>
2019-07-08 13:29:55 +0200

    Add routes for games

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :pomodoros, only: %i[create]
-
-  # Sidekiq Web UI, only for admins.
-  require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
+  resources :games, only: %i[create]
 end
