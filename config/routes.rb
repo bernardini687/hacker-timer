ff30a0abd335065e9a678751b54facc280ea1dee
Oscar Bernardini <bernardini687@gmail.com>
2019-07-08 13:19:03 +0200

    Remove authentication from routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,5 @@ Rails.application.routes.draw do
 
   # Sidekiq Web UI, only for admins.
   require 'sidekiq/web'
-  # authenticate :user, lambda { |u| u.admin } do
-  authenticate :user do
-    mount Sidekiq::Web => '/sidekiq'
-  end
+  mount Sidekiq::Web => '/sidekiq'
 end
