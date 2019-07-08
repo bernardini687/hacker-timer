de37945cf2a449e0c3b6f81a52db0e4a55f3f000
Oscar Bernardini <bernardini687@gmail.com>
2019-07-08 13:28:35 +0200

    Add validations to Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,15 @@
 class Game < ApplicationRecord
+  validates :number_of_players, numericality: {
+    only_integer: true,
+    greater_or_equal_to: 3,
+    less_than_or_equal_to: 8
+  }
+
+  # after_save :create_players
+
+  # private
+
+  # def create_players
+  #   number_of_players.times { Player.new }
+  # end
 end
