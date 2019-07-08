aa99f943cc398f7e8070f3458f7e8e1fca00ea66
Oscar Bernardini <bernardini687@gmail.com>
2019-07-08 14:06:01 +0200

    Write Game specifications

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,11 @@
 class Game < ApplicationRecord
   validates :number_of_players, numericality: {
     only_integer: true,
-    greater_or_equal_to: 3,
+    greater_than_or_equal_to: 3,
     less_than_or_equal_to: 8
   }
+
+  has_many :players
 
   # after_save :create_players
 
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is accessible' do
+    game = Game.create!(number_of_players: 5)
+    expect(game).to eq(Game.last)
+  end
+
+  it 'has number_of_players column' do
+    columns = Game.column_names
+    expect(columns).to include('id')
+    expect(columns).to include('number_of_players')
+    expect(columns).not_to include('user_id')
+  end
+
+  it 'has number_of_players column that defaults to 3' do
+    expect(Game.new.number_of_players).to eq 3
+  end
+
+  it 'validates number_of_players' do
+    expect(Game.new(number_of_players: 2)).not_to be_valid
+    expect(Game.new(number_of_players: 9)).not_to be_valid
+    expect(Game.new(number_of_players: 5)).to be_valid
+    expect(Game.new).to be_valid
+  end
+
+  it 'generates the correct number of players' do
+    game = Game.create!(number_of_players: 5)
+
+    expect(game.number_of_players).to eq(game.players.count)
+  end
 end
