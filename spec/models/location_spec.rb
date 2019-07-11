7e47e7c97442b967ed54f726420f283b80653477
Oscar Bernardini <bernardini687@gmail.com>
2019-07-11 10:25:08 +0200

    Test locations' validity

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,8 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Location, type: :model do
   it 'is accessible' do
-    location = Location.create!(name: 'Circus')
+    location = Location.create!(name: 'circus')
 
-    expect(player).to eq(Location.last)
+    expect(location).to eq(Location.last)
+  end
+
+  it 'has name column' do
+    columns = Location.column_names
+
+    expect(columns).to include('name')
+  end
+
+  it 'validates name presence' do
+    expect(Location.new).not_to be_valid
+    expect(Location.new(name: 'circus')).to be_valid
   end
 end
