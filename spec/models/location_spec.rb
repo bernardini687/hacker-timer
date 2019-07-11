c1904d6efe862c94bc5d415d60199603d4708733
Oscar Bernardini <bernardini687@gmail.com>
2019-07-11 10:18:27 +0200

    Start locations test

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is accessible' do
+    location = Location.create!(name: 'Circus')
+
+    expect(player).to eq(Location.last)
+  end
 end
